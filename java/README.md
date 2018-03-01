ed7f67b9272b60a4fcf347c3a95ceeb42822e20f
Supasate Choochaisri <supasate@fb.com>
2018-03-01 22:15:32 +0800

    Add Java demo

---
+++ java/README.md
@@ -0,0 +1,44 @@
+Facebook Realtime Lead Ads Demo (Java)
+=========================================
+A Java demo for retrieving realtime leads from Facebook lead ads
+
+Prerequisites
+=============
+1. HTTPS domain and HTTPS endpoint must be accessible from Facebook
+2. A page admin's access token with `manage_pages` permission
+
+Getting Started
+===============
+1. Copy the file `token/access_token.txt.template` to `token/access_token.txt` and change its content to your system user access token. 
+
+Note: Your system user needs to be a page admin and grant `manage_pages` permission.
+
+2. Build with gradle
+```base
+./gradlew build
+````
+
+3. Run
+```bash
+./gradlew run
+``` 
+
+How to test with your local machine
+===================================
+If you don't have a HTTPS domain, you can test it with your local machine by using ngrok (or other https tunneling service).
+
+1. Install ngrok.
+```bash
+npm install -g ngrok
+```
+2. Start ngrok tunneling to forward http(s) traffic to port 3000 (or your preferred port).
+```bash
+ngrok http 3000
+```
+
+3. It will print its status with a line like the following:
+```bash
+Forwarding                    https://7f05e4bb.ngrok.io -> localhost:3000
+```
+Copy this https URL. We will use it to set up a webhook.
+
---
+++ java/src/main/java/LeadGenWebhook.java
@@ -0,0 +1,100 @@
+import io.javalin.Javalin;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URL;
+import java.util.Scanner;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+
+public class LeadGenWebhook {
+    public static final int SERVER_PORT = 3000;
+
+    public static void main(String[] args) {
+        Javalin app = Javalin.start(SERVER_PORT);
+        /////////////////////////////////////////////////////////////////////////
+        //           Part 1: Subscribe a leadgen endpoint to webhook           //
+        /////////////////////////////////////////////////////////////////////////
+        // A token that Facebook will echo back to you as part of callback URL verification.
+        String VERIFY_TOKEN = "YOUR_SECURE_VERIFY_TOKEN";
+        // Endpoint for verifying webhook subscripton
+        app.get("/leadgen", ctx -> {
+            if (ctx.anyQueryParamNull("hub.verify_token", "hub.challenge")) {
+              ctx.result("Missing hub.verify_token and hub.challenge params");
+              return;
+            }
+            String verifyToken = ctx.queryParam("hub.verify_token");
+            String challenge = ctx.queryParam("hub.challenge");
+
+            if (!verifyToken.equals(VERIFY_TOKEN)) {
+                ctx.result("Verify token does not match");
+                return;
+            }
+
+            ctx.result(challenge);
+        });
+
+        /////////////////////////////////////////////////////////////////////////
+        //                  Part 2: Retrieving realtime leads                  //
+        /////////////////////////////////////////////////////////////////////////
+        // Graph API endpoint
+        String GRAPH_API_VERSION = "v2.12";
+        String GRAPH_API_ENDPOINT = "https://graph.facebook.com/" + GRAPH_API_VERSION;
+        // Your system user access token file path.
+        // Note: Your system user needs to be an admin of the subscribed page.
+        String ACCESS_TOKEN_PATH = System.getProperty("user.dir") + "/token/access_token.txt";
+        // Read access token for calling graph API.
+        String accessTokenBuffer = null;
+        try {
+            Scanner sc = new Scanner(new File(ACCESS_TOKEN_PATH));
+            accessTokenBuffer = sc.next();
+        } catch (FileNotFoundException error) {
+            System.out.println("The access token file does not exist. " + ACCESS_TOKEN_PATH);
+            System.exit(0);
+        }
+        final String accessToken = accessTokenBuffer;
+        // Facebook will post realtime leads to this endpoint if we've already subscribed to the webhook in part 1.
+        app.post("/leadgen", ctx -> {
+            JSONObject body = new JSONObject(ctx.body());
+            JSONArray entry = body.getJSONArray("entry");
+
+            for (int i = 0; i < entry.length(); i++) {
+                JSONArray changes = entry.getJSONObject(i).getJSONArray("changes");
+                for (int j = 0; j < changes.length(); j++) {
+                    JSONObject change = changes.getJSONObject(j).getJSONObject("value");
+                    // We get page, form, and lead IDs from the change here.
+                    // We need the lead gen ID to get the lead data.
+                    // The form ID and page ID are optional. You may want to record them into your CRM system.
+                    String pageID = change.getString("page_id");
+                    String formID = change.getString("form_id");
+                    String leadgenID = change.getString("leadgen_id");
+                    System.out.println("Page ID " + pageID + ", Form ID " + formID + ", Lead gen ID " + leadgenID);
+
+                    // Call graph API to request lead info with the lead ID and access token.
+                    String leadgenURI = GRAPH_API_ENDPOINT + "/" + leadgenID + "?access_token=" + accessToken;
+                    Scanner urlScanner = new Scanner(new URL(leadgenURI).openStream());
+                    // “\A” is a regex for the beginning of the input.
+                    // next() will return from the beginning all the way to the end.
+                    JSONObject response = new JSONObject(urlScanner.useDelimiter("\\A").next());
+
+                    String createdTime = response.getString("created_time");
+                    String id = response.getString("id");
+                    JSONArray fields = response.getJSONArray("field_data");
+
+                    // Handle lead answer here (insert data into your CRM system)
+                    System.out.println("Lead ID " + id);
+                    System.out.println("Created time " + createdTime);
+                    for (int k = 0; k < fields.length(); k++) {
+                      JSONObject field = fields.getJSONObject(k);
+                      String question = field.getString("name");
+                      JSONArray answers = field.getJSONArray("values");
+
+                      System.out.println("Question " + question);
+                      System.out.println("Answers " + answers.join(","));
+                    }
+                }
+            }
+            ctx.status(200);
+        });
+    }
+}
