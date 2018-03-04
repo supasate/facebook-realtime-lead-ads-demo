b8a575c167e703c1b4091576ad60cc2623146720
Supasate Choochaisri <supasate@fb.com>
2018-03-04 15:52:13 +0800

    Add Ruby demo

---
+++ ruby/README.md
@@ -0,0 +1,44 @@
+Facebook Realtime Lead Ads Demo (Ruby)
+=========================================
+A Ruby demo for retrieving realtime leads from Facebook lead ads
+
+Prerequisites
+=============
+1. HTTPS domain and HTTPS endpoint must be accessible from Facebook
+2. A page admin's access token with `manage_pages` permission
+
+Getting Started
+===============
+1. Install packages with [bundle](http://bundler.io/)
+```bash
+bundle install
+```
+
+2. Copy the file `token/access_token.txt.template` to `token/access_token.txt` and change its content to your system user access token. 
+
+Note: Your system user needs to be a page admin and grant `manage_pages` permission.
+
+3. Start
+```bash
+ruby src/app.rb
+``` 
+
+How to test with your local machine
+===================================
+If you don't have a HTTPS domain, you can test it with your local machine by using ngrok (or other https tunneling service).
+
+1. Install ngrok.
+```bash
+npm install -g ngrok
+```
+2. Start ngrok tunneling to forward http(s) traffic to port 3000 (or your preferred port).
+```bash
+ngrok http 3000
+```
+
+3. It will print its status with a line like the following:
+```bash
+Forwarding                    https://7f05e4bb.ngrok.io -> localhost:3000
+```
+Copy this https URL. We will use it to set up a webhook.
+
---
+++ ruby/src/app.rb
@@ -0,0 +1,81 @@
+require 'sinatra'
+require 'net/http'
+
+set :port, 3000
+
+###################################################################
+#         Part 1: Subscribe a leadgen endpoint to webhook         #
+###################################################################
+# A token that Facebook will echo back to you as part of
+# callback URL verification.
+VERIFY_TOKEN = 'YOUR_SECURE_VERIFY_TOKEN'
+# Endpoint for verifying webhook subscripton
+get '/leadgen' do
+  # Extract a verify token we set in the webhook subscription
+  # and a challenge to echo back.
+  verify_token = params['hub.verify_token']
+  challenge = params['hub.challenge']
+
+  if verify_token.nil? || challenge.nil?
+    return 'Missing hub.verify_token and hub.challenge params'
+  end
+
+  if verify_token != VERIFY_TOKEN
+    return 'Verify token does not match'
+  end
+  # We echo the received challenge back to Facebook to finish
+  # the verification process.
+  return challenge
+end
+
+###################################################################
+#               Part 2: Retrieving realtime leads                 #
+###################################################################
+# Your system user access token file path.
+# Note: Your system user needs to be an admin of the subscribed page.
+ACCESS_TOKEN_PATH = File.join(File.dirname(__FILE__), '..', 'token', 'access_token.txt')
+# Graph API endpoint
+GRAPH_API_VERSION = 'v2.12'
+GRAPH_API_ENDPOINT = "https://graph.facebook.com/#{GRAPH_API_VERSION}"
+
+# Facebook will post realtime leads to this endpoint
+# if we've already subscribed to the webhook in part 1.
+post '/leadgen' do
+  if !File.file?(ACCESS_TOKEN_PATH)
+    puts 'Access token file does not exsist'
+    return 500
+  end
+
+  access_token = File.read(ACCESS_TOKEN_PATH)
+
+  # Get value from POST request body
+  body = JSON.parse request.body.read
+  body['entry'].each { |page|
+    page['changes'].each { |change|
+      # We get page, form, and lead IDs from the change here.
+      # We need the lead gen ID to get the lead data.
+      # The form ID and page ID are optional.
+      # You may want to record them into your CRM system.
+      page_id = change['value']['page_id']
+      form_id = change['value']['form_id']
+      leadgen_id = change['value']['leadgen_id']
+      puts "Page ID #{page_id}, Form ID #{form_id}, Leadgen ID #{leadgen_id}"
+      # Call graph API to request lead info with the lead ID
+      # and access token.
+      leadgen_uri = "#{GRAPH_API_ENDPOINT}/#{leadgen_id}?access_token=#{access_token}"
+      response = JSON.parse Net::HTTP.get(URI.parse(leadgen_uri))
+      created_time = response['created_time']
+      field_data = response['field_data']
+
+      # Handle lead answer here (insert data into your CRM system)
+      puts "Created time #{created_time}"
+      field_data.each { |field|
+        question = field['name']
+        answers = field['values'].join(',')
+        puts "Question #{question}"
+        puts "Answer #{answers}"
+      }
+    }
+  }
+  return 200
+end
